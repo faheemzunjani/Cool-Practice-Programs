05e5ad1b7ffda8f43d57d51d8afd1fac98e0c5bf
Faheem Zunjani <faheemzunjani@gmail.com>
2016-04-04 15:26:08 +0530

    Add lab2_1.c

---
+++ Introductory-Programs/lab2_1.c
@@ -0,0 +1,17 @@
+// Program to find the value of (x^2*y)/z
+// Faheem Hassan Zunjani
+
+#include<stdio.h>
+
+void main(){
+  
+  float x,y,z;
+
+  printf("Enter x,y,z:");	
+  scanf("%f,%f,%f",&x,&y,&z);	
+
+  printf("\n((x^2)*y)/z is: %f \n",((x*x*y)/z));
+	
+}
+
+	
