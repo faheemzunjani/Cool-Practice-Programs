ac0c73f85bad01b812a20c45cedef1de4634ffea
Faheem Zunjani <faheemzunjani@gmail.com>
2016-12-17 18:09:16 +0530

    Add AAP Lab 6 codes

---
+++ DSA-Programs/lab6_aap3_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct node {
+	int info;
+	struct node * next;
+};
+
+struct node * insertion_sort(struct node * head);
+
+int main()
+{
+	struct node * head = NULL;
+	
+	return 0;
+}
+
+struct node * insertion_sort(struct node * head)
+{
+}
